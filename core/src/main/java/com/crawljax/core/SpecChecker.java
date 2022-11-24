f853a4e3bed5149c12347e1d1158c35bc8fca2cf
Ido Greenspan <ido.greenspan@gmail.com>
2022-11-24 09:27:49 +0000

    +

--- core/src/main/java/com/crawljax/core/SpecChecker.java
+++ core/src/main/java/com/crawljax/core/SpecChecker.java
@@ -11,29 +11,29 @@ import com.crawljax.core.state.StateVertex;
 // needs to be created using a singleton: only one instance required.
 public class SpecChecker {
 
-    private List<StateConstraints> list; // the name will be changed
+    private List<StateConstraints> stateConstraintsList;
     private int index;
 
     /**
-     * @param list             - a list of StateConstraints
+     * @param stateConstraintsList - a list of StateConstraints
      * @param index - the index in the ArrayList of the current
      *                         DOM state constraints
      */
 
     // constructor
-    public SpecChecker(List<StateConstraints> list) {
-        this.list = new ArrayList<StateConstraints>(list);
+    public SpecChecker(List<StateConstraints> stateConstraintsList) {
+        this.stateConstraintsList = new ArrayList<StateConstraints>(stateConstraintsList);
         this.index = 0; // the head of the list
     }
 
     /**
      * @param state - the current DOM state
-     * @throws IOException
-     * @throws XPathExpressionException
+     * @throws IOException - failed or interrupted I/O operations
+     * @throws XPathExpressionException - error in an XPath expression
      */
     public boolean isSatisfied(StateVertex state) throws IOException, XPathExpressionException
     {
-        if(list.get(index).isSatisfied(state.getDocument())){
+        if(stateConstraintsList.get(index).isSatisfied(state.getDocument())){
             index++; // move the pointer to the next stateConstraints
             return true;
         }
